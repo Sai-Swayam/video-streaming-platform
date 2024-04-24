15ba5c4f1f2c06cfdc95b2f4242fd9d9899cf24e
Sai Swayam Shree <saiswayamshree19@gmail.com>
2024-04-24 13:00:32 +0530

    Util code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
-    path:"./env"
-})
+  path: "./env",
+});
+
+// connectDB() is a function made with async and await, which returns a promise. We are using
+// that promise below with the help of .then and .catch.
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MongoDB Connection Error: ${error}`);
+  });
 
 
-connectDB();
-
-
-
-
-
-
-
-
+  
 /*
     // The code below is the first approach to connect to the database using mongoose    
 
